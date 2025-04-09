9cb44f86c16128fb318cb1493f1385b1908ea790
josecipriano <ciprianoaceytunoj@gmail.com>
2025-04-08 21:29:04 -0600

    RegisterAndLoginCompleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Route, useRoutes } from "react-router-dom"
-import { Toaster } from "react-hot-toast"
-import routes from "./routes.jsx"
+import { useRoutes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import routes from "./routes.jsx";
 
 
 export const App = () => {
-  let element =useRoutes(routes);
+
+  let element = useRoutes(routes);
+
   return (
     <>
       {element}
       <Toaster
-      position = "bottom-right"
-      reverseOrder = {false}
+        position="bottom-right"
+        reverseOrder={false}
       />
-
     </>
-
-  ) 
+  )
 }
